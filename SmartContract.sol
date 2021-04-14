6d2816ba5f8fdbcde52a63af82a3d76f2bdea7c7
salwaAlhajjaji <salwa.alhajjaji@hotmail.com>
2021-04-14 15:58:54 +0300

    Update SmartContract.sol

--- SmartContract.sol
+++ SmartContract.sol
@@ -1,12 +1,12 @@
 pragma solidity >=0.6.0 <0.8.1;
 
-import "../.deps/github/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
+// import "../.deps/github/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
 
-contract gift is ERC1155 {
+contract gift {
     
     string private _symbol;
     string private  _name;
-   constructor() public ERC1155("") {
+   constructor() public {
         // add varible need to set while cotract run
         _name = "Bravo";
         _symbol = "BRV";
@@ -34,17 +34,26 @@ contract gift is ERC1155 {
     uint _cardId;
     mapping (uint => Card) public cards;
     mapping (address => reward) public rewards;
-    // mapping (uint256 => mapping(address => uint256)) private _balances;
-    
-       function name() public view returns (string memory) {
+    mapping (uint256 => mapping(address => uint256)) private _balances;
+
+    function name() public view returns (string memory) {
         return _name;
     }
 
     function symbol() public view  returns (string memory) {
         return _symbol;
     }
-    function issueGiftCard(address _beneficiary,uint _value) public payable {
+    
+    function buyGiftCard(address _beneficiary, uint total) public payable{
+        //Need to calc fees and total in the website
+        require(total == msg.value, "Check your balance");
+        uint _value = total - (total*2/100);
+        issueGiftCard(_beneficiary, _value);
+    }
+    
+    function issueGiftCard(address _beneficiary,uint _value) private{
         // REQUIRE
+        //REQUIRE for min&max value 
         // Generate randome number for the card ID
         Card memory newCard = Card (_value, block.timestamp, block.timestamp + 365 days, _beneficiary,msg.sender);
         cards[_cardId] = newCard;
@@ -62,8 +71,9 @@ contract gift is ERC1155 {
          safeTransferFrom (msg.sender,_newBeneficiary,_cardId, cards[_cardId].value,"");
          cards[_cardId].beneficiary = _newBeneficiary;
      }
-     
+
      function addFundsToGiftCard(uint _cardId, uint _amount) public payable{
+        //  calc fees *********
          //  REQUIRE Owner
          cards[_cardId].value+=_amount;
         //  _balances[_cardId][msg.sender]+=_amount;
@@ -104,17 +114,84 @@ contract gift is ERC1155 {
     }
        Card [] public  mycards ;
      function myCards() public returns (Card[] memory) {
-       
-         uint counter;
+        //  uint counter;
          for (uint i=0; i< 10 ; i++ ){
              if(cards[i].beneficiary==msg.sender){
-                 mycards[counter ++]=cards[i];
+                 mycards.push(cards[i]);
              }
-             
          }
          return mycards;
      }
   
- 
+ ////////ERC function
+     function _mint(address account, uint256 id, uint256 amount, bytes memory data) internal  {
+        require(account != address(0), "ERC1155: mint to the zero address");
+        address operator = msg.sender;
+        // address operator = _msgSender();
+
+        _beforeTokenTransfer(operator, address(0), account, _asSingletonArray(id), _asSingletonArray(amount), data);
+
+        _balances[id][account] += amount;
+        emit TransferSingle(operator, address(0), account, id, amount);
+
+        _doSafeTransferAcceptanceCheck(operator, address(0), account, id, amount, data);
+    }
+        function safeTransferFrom(address from, address to, uint256 id, uint256 amount, bytes memory data) public {
+        require(to != address(0), "ERC1155: transfer to the zero address");
+        require(from == _msgSender() || isApprovedForAll(from, _msgSender()),
+            "ERC1155: caller is not owner nor approved");
+
+        // address operator = _msgSender();
+        address operator = msg.sender;
+
+
+        _beforeTokenTransfer(operator, from, to, _asSingletonArray(id), _asSingletonArray(amount), data);
+
+        uint256 fromBalance = _balances[id][from];
+        require(fromBalance >= amount, "ERC1155: insufficient balance for transfer");
+        _balances[id][from] = fromBalance - amount;
+        _balances[id][to] += amount;
+
+        emit TransferSingle(operator, from, to, id, amount);
+
+        _doSafeTransferAcceptanceCheck(operator, from, to, id, amount, data);
+    }
+        function _burn(address account, uint256 id, uint256 amount) internal {
+        require(account != address(0), "ERC1155: burn from the zero address");
+        address operator = msg.sender;
+        // address operator = _msgSender();
+
+        _beforeTokenTransfer(operator, account, address(0), _asSingletonArray(id), _asSingletonArray(amount), "");
+
+        uint256 accountBalance = _balances[id][account];
+        require(accountBalance >= amount, "ERC1155: burn amount exceeds balance");
+        _balances[id][account] = accountBalance - amount;
+
+        emit TransferSingle(operator, account, address(0), id, amount);
+    }
+        function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) internal{ 
+            
+        }
+        
+    function _asSingletonArray(uint256 element) private pure returns (uint256[] memory) {
+        uint256[] memory array = new uint256[](1);
+        array[0] = element;
+
+        return array;
+    }
+     function _doSafeTransferAcceptanceCheck(address operator, address from, address to, uint256 id, uint256 amount, bytes memory data) private {
+        if (to.isContract()) {
+            try IERC1155Receiver(to).onERC1155Received(operator, from, id, amount, data) returns (bytes4 response) {
+                if (response != IERC1155Receiver(to).onERC1155Received.selector) {
+                    revert("ERC1155: ERC1155Receiver rejected tokens");
+                }
+            } catch Error(string memory reason) {
+                revert(reason);
+            } catch {
+                revert("ERC1155: transfer to non ERC1155Receiver implementer");
+            }
+        }
+    }
+
      
 }
